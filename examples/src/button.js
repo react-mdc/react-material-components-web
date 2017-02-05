7fba6e47f26f304242a807d51784eeaf41b37f00
Choi Geonu <6566gun@gmail.com>
2017-02-05 20:05:35 +0900

    Add ripple button example

--- examples/src/button.js
+++ examples/src/button.js
@@ -1,8 +1,50 @@
 /* @flow */
 import React from 'react';
 
+import {Ripple} from 'react-mdcw/lib/ripple';
 import {Button} from 'react-mdcw/lib/button';
 import {Title, Typography} from 'react-mdcw/lib/typography';
+
+function FieldSet (props: *): * {
+  let {style} = props;
+  style = {
+    ...style,
+    witdh: 500,
+    margin: 16
+  };
+  return (
+    <fieldset
+      {...props}
+      style={style} />
+  );
+}
+
+function ButtonDemo (props: *): * {
+  let {style} = props;
+  style = {
+    ...style,
+    display: 'inline-block',
+    minWidth: 100,
+    padding: 16
+  };
+  return (
+    <div
+      {...props}
+      style={style} />
+  );
+}
+
+function RippleButton (props: *): * {
+  let {
+    wrap,
+    ...rippleProps
+  } = props;
+  let buttonProps = {};
+  if (wrap != null) {
+    buttonProps['wrap'] = wrap;
+  }
+  return (<Ripple wrap={<Button {...buttonProps} />} {...rippleProps} />);
+}
 
 export default class ButtonExample extends React.Component {
   render (): React.Element<*> {
@@ -11,75 +53,111 @@ export default class ButtonExample extends React.Component {
         <Title>
           Button Examples
         </Title>
-        <fieldset>
+        <FieldSet>
           <legend>Buttons</legend>
-          <div style={{margin: 16}}>
+          <ButtonDemo>
             <Button>DEFAULT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button raised>RAISED</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button dense>DENSE DEFAULT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button dense raised>DENSE RAISED</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button compact>COMPACT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button compact raised>COMPACT RAISED</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button primary>DEFAULT WITH PRIMARY</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button raised primary>RAISED WITH PRIMARY</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button accent>DEFAULT WITH ACCENT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button raised accent>RAISED WITH ACCENT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button wrap={<div />}>DIV</Button>
-          </div>
-        </fieldset>
-        <fieldset>
+          </ButtonDemo>
+        </FieldSet>
+        <FieldSet>
           <legend>Disabled Buttons</legend>
-          <div style={{margin: 16}}>
+          <ButtonDemo>
             <Button disabled>DEFAULT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled raised>RAISED</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled dense>DENSE DEFAULT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled dense raised>DENSE RAISED</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled compact>COMPACT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled compact raised>COMPACT RAISED</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled primary>DEFAULT WITH PRIMARY</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled raised primary>RAISED WITH PRIMARY</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled accent>DEFAULT WITH ACCENT</Button>
-          </div>
-          <div style={{margin: 16}}>
+          </ButtonDemo>
+          <ButtonDemo>
             <Button disabled raised accent>RAISED WITH ACCENT</Button>
-          </div>
-        </fieldset>
+          </ButtonDemo>
+        </FieldSet>
+        <FieldSet>
+          <legend>Ripple Buttons</legend>
+          <ButtonDemo>
+            <RippleButton>DEFAULT</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton raised>RAISED</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton dense>DENSE DEFAULT</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton dense raised>DENSE RAISED</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton compact>COMPACT</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton compact raised>COMPACT RAISED</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton primary>DEFAULT WITH PRIMARY</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton raised primary>RAISED WITH PRIMARY</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton accent>DEFAULT WITH ACCENT</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton raised accent>RAISED WITH ACCENT</RippleButton>
+          </ButtonDemo>
+          <ButtonDemo>
+            <RippleButton wrap={<div />}>DIV</RippleButton>
+          </ButtonDemo>
+        </FieldSet>
       </Typography>
     );
   }
