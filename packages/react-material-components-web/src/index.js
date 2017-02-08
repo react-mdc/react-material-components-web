b8b8a3f2eea63ab235c153c4b11c40607fd62d20
Choi Geonu <6566gun@gmail.com>
2017-02-08 13:51:17 +0900

    Add react-material-components-web package

---
+++ packages/react-material-components-web/src/index.js
@@ -0,0 +1,31 @@
+/* @flow */
+import * as base from '@react-mdc/base';
+import * as card from '@react-mdc/card';
+import * as drawer from '@react-mdc/drawer';
+import * as fab from '@react-mdc/fab';
+import * as list from '@react-mdc/list';
+import * as theme from '@react-mdc/theme';
+import * as button from '@react-mdc/button';
+import * as checkbox from '@react-mdc/checkbox';
+import * as elevation from '@react-mdc/elevation';
+import * as formField from '@react-mdc/form-field';
+import * as radio from '@react-mdc/radio';
+import * as ripple from '@react-mdc/ripple';
+import * as typography from '@react-mdc/typography';
+
+// Export all
+export {
+  base,
+  card,
+  drawer,
+  fab,
+  list,
+  theme,
+  button,
+  checkbox,
+  elevation,
+  formField,
+  radio,
+  ripple,
+  typography
+};
