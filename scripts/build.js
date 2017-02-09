7c92c5c2a83857aa443ac1577b987e74e0b7acf9
Choi Geonu <6566gun@gmail.com>
2017-02-10 01:51:35 +0900

    Add single file / package build

--- scripts/build.js
+++ scripts/build.js
@@ -1,48 +1,108 @@
 'use strict';
 
 const path = require('path');
+const program = require('commander');
 const chalk = require('chalk');
 
 const util = require('./util');
 
-const SRC_DIRECTORY = 'src';
-const OUT_DIRECTORY = 'lib';
-
-function buildPackage (name, directory, src = SRC_DIRECTORY, out = OUT_DIRECTORY) {
-  const srcDirectory = path.resolve(directory, src);
-  const outDirectory = path.resolve(directory, out);
-
-  console.log(chalk.green(`Building package: ${name}`));
-
-  let result = util.runCommand('babel', [
-    srcDirectory, '-d', outDirectory
-  ]);
-  if (result !== 0) {
-    console.warn(chalk.yellow(`Failed to build ${name}, babel exit code: ${result}`));
-    return false;
-  }
-  result = util.runCommand('flow-copy-source', [
-    srcDirectory, outDirectory
-  ]);
-  if (result !== 0) {
-    console.warn(chalk.yellow(`Failed to build ${name}, flow-copy-source exit code: ${result}`));
-    return false;
-  }
-  return true;
-}
-
-function buildAllPackages () {
-  const packages = util.getPackages();
-  return Object.keys(packages).every((name) => {
-    const directory = packages[name];
-    return buildPackage(name, directory);
+function copyFlowSingleSource (srcPath, outPath) {
+  const flowPath = outPath + '.flow';
+  return new Promise((resolve, reject) => {
+    util.copyFile(srcPath, flowPath, (error) => {
+      if (error != null) {
+        reject(error);
+      } else {
+        resolve();
+      }
+    });
   });
 }
 
-function main () {
-  if (!buildAllPackages()) {
-    process.exit(1);
+function runBuildFile (srcPath, outPath) {
+  return util.runLocalCommand('babel', [
+    srcPath, '-o', outPath
+  ]).then(() => true, (code) => {
+    console.error(chalk.red(`Failed to build ${srcPath}, babel exit code: ${code}`));
+    Promise.reject();
+  }).then(() => copyFlowSingleSource(srcPath, outPath))
+    .then(() => {
+      console.log(`${srcPath} -> ${outPath}.flow`);
+      return true;
+    }, (error) => {
+      console.error(chalk.red(`Failed to build ${srcPath}, failed to copy flow source.`));
+      Promise.reject(error);
+    });
+}
+
+function runBuildDirectory (srcPath, outPath) {
+  return util.runLocalCommand('babel', [
+    srcPath, '-d', outPath
+  ]).then(() => true, (code) => {
+    console.error(chalk.red(`Failed to build ${srcPath}, babel exit code: ${code}`));
+    Promise.reject();
+  }).then(() => util.runLocalCommand('flow-copy-source', [
+    srcPath, outPath
+  ]).then(() => true, (code) => {
+    console.error(chalk.red(`Failed to build ${srcPath}, flow-copy-source exit code: ${code}`));
+    Promise.reject();
+  }));
+}
+
+function buildPath (target) {
+  const pkg = util.findPackage(target);
+  if (pkg == null) {
+    console.error(chalk.red(`${target} it not included in any package`));
+    return false;
   }
+  const srcPath = path.resolve(target);
+  const outPath = util.getOutPath(pkg, srcPath);
+  console.log(chalk.green(`Building "${target}" in package: ${pkg.name}`));
+  return runBuildFile(srcPath, outPath);
+}
+
+function buildPackage (pkg) {
+  const {name, srcPath, outPath} = pkg;
+
+  console.log(chalk.green(`Building package: ${name}`));
+  return runBuildDirectory(srcPath, outPath).catch((e) => {
+    console.error(chalk.red(`Failed to build package: ${name}`));
+    Promise.reject(e);
+  });
+}
+
+function buildPackages (packages) {
+  return packages
+    .slice(1)
+    .reduce((p, pkg) => p.then(() => buildPackage(pkg)), buildPackage(packages[0]));
+}
+
+function runCommand (target) {
+  const allPackages = util.getPackages();
+
+  if (target == null) {
+    return buildPackages(allPackages);
+  }
+  const pkg = allPackages.find(x => x.name === target);
+  if (pkg != null) {
+    return buildPackage(pkg);
+  } else {
+    return buildPath(target);
+  }
+}
+
+function main () {
+  let args = {};
+  program
+    .arguments('[target]')
+    .action((target) => {
+      args.target = target;
+    });
+
+  program.parse(process.argv);
+
+  let {target} = args;
+  runCommand(target);
 }
 
 main();
--- scripts/util.js
+++ scripts/util.js
@@ -4,31 +4,133 @@ const _ = require('lodash');
 const chalk = require('chalk');
 const fs = require('fs');
 const path = require('path');
-const spawnSync = require('child_process').spawnSync;
+const {spawn} = require('child_process');
 
 const PROJECT_ROOT = require('./constants').PROJECT_ROOT;
 
-const PACKAGES_DIR = path.resolve(__dirname, '../packages');
+const NODE_MODULES_DIR = path.resolve(PROJECT_ROOT, 'node_modules');
 
-module.exports.getPackages = function (packagesDir = PACKAGES_DIR) {
-  return fs.readdirSync(packagesDir)
-    .map(filename => [filename, path.resolve(packagesDir, filename)])
-    .filter(([_name, file]) => fs.lstatSync(path.resolve(file)).isDirectory())
-    .reduce((x, [name, file]) => _.assign({}, x, {[name]: file}), {});
+const LOCAL_BIN_DIR = path.resolve(NODE_MODULES_DIR, '.bin');
+
+const PACKAGES_DIR = path.resolve(PROJECT_ROOT, 'packages');
+
+const SRC_DIRECTORY = 'src';
+
+const OUT_DIRECTORY = 'lib';
+
+/**
+ * Get package object from source file
+ */
+module.exports.findPackage = function (sourceFilename) {
+  const packageDir = packageDirectory(sourceFilename);
+  const srcDir = path.resolve(packageDir, SRC_DIRECTORY);
+  const relative = path.relative(srcDir, sourceFilename);
+  if (relative.includes('..')) {
+    return null;
+  }
+  return getPackage(packageDir);
 };
 
-module.exports.runCommand = function (command, args, opts = {}) {
+/**
+ * Get package object from source file
+ */
+const getPackage = module.exports.getPackage = function (filename) {
+  const name = path.relative(PACKAGES_DIR, filename);
+  if (name.includes('..') || name.includes('/')) {
+    return null;
+  }
+  const packageDir = path.resolve(PACKAGES_DIR, name);
+  return {
+    name,
+    path: packageDir,
+    srcPath: path.resolve(packageDir, SRC_DIRECTORY),
+    outPath: path.resolve(packageDir, OUT_DIRECTORY)
+  };
+};
+
+/**
+ * Get package directory of file.
+ */
+const packageDirectory = module.exports.packageDirectory = function (filename) {
+  const name = path.relative(PACKAGES_DIR, filename).split('/')[0];
+  if (name.includes('..')) {
+    return null;
+  }
+  return path.resolve(PACKAGES_DIR, name);
+};
+
+/**
+ * Get build out path for source
+ */
+module.exports.getOutPath = function (pkg, filename) {
+  const srcRelative = path.relative(pkg.srcPath, filename);
+  return path.resolve(pkg.outPath, srcRelative);
+};
+
+module.exports.getPackages = function () {
+  return fs.readdirSync(PACKAGES_DIR)
+    .map(filename => path.resolve(PACKAGES_DIR, filename))
+    .filter((filename) => fs.lstatSync(path.resolve(filename)).isDirectory())
+    .map(getPackage);
+};
+
+const runCommand = module.exports.runCommand = function (command, args = [], opts = {}) {
+  let commandName = path.relative(LOCAL_BIN_DIR, command);
+  if (commandName.includes('..')) {
+    commandName = command;
+  }
   opts = _.assign({}, {
-    'cwd': PROJECT_ROOT,
-    'stdio': [0, 1, 1]
+    'cwd': PROJECT_ROOT
   }, opts);
 
-  const result = spawnSync(command, args, opts);
+  console.log(chalk.cyan(`Run command ${commandName} ${args.join(' ')}`));
 
-  if (result.error || result.status !== 0) {
-    if (result.error != null) {
-      console.error(chalk.red(result.error));
+  const spawned = spawn(command, args, opts);
+
+  spawned.stdout.on('data', (data) => {
+    process.stdout.write(data);
+  });
+
+  spawned.stderr.on('data', (data) => {
+    process.stderr.write(chalk.red(data.toString()));
+  });
+
+  return new Promise((resolve, reject) => {
+    spawned.on('close', (code) => {
+      if (code === 0) {
+        resolve(code);
+      } else {
+        reject(code);
+      }
+    });
+  });
+};
+
+module.exports.runLocalCommand = function (command, args, opts = {}) {
+  command = path.resolve(LOCAL_BIN_DIR, command);
+  return runCommand(command, args, opts);
+};
+
+module.exports.copyFile = function (source, target, cb) {
+  let cbCalled = false;
+
+  const readStream = fs.createReadStream(source);
+  readStream.on('error', function (err) {
+    done(err);
+  });
+  const writeStream = fs.createWriteStream(target);
+  writeStream.on('error', function (err) {
+    done(err);
+  });
+  writeStream.on('close', function () {
+    done();
+  });
+  readStream.pipe(writeStream);
+
+  function done (err) {
+    if (!cbCalled) {
+      cb(err);
+      cbCalled = true;
     }
   }
-  return result.status;
 };
