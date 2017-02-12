0a7f2505f4d2c504968eeb32c1e35e4a85ed21bd
Choi Geonu <6566gun@gmail.com>
2017-02-13 02:01:32 +0900

    @react-mdc/drawer: apply NativeDOMAdapter / share adapter using context

--- packages/drawer/src/temporary/drawer.js
+++ packages/drawer/src/temporary/drawer.js
@@ -2,28 +2,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import classNames from 'classnames';
+import {MDCTemporaryDrawerFoundation} from '@material/drawer/dist/mdc.drawer';
+import {
+  Set,
+  OrderedSet,
+  Map
+} from 'immutable';
 
 import type {Props as WrapperProps} from '@react-mdc/base/lib/wrapper';
-import {PropWrapper} from '@react-mdc/base';
+import {
+  PropWrapper,
+  NativeDOMAdapter
+} from '@react-mdc/base';
 import {eventHandlerDecorator} from '@react-mdc/base/lib/util';
 
-import type {AdapterDrawerDelegate, AdapterDrawerCallback} from './types';
+import {FoundationAdapter, DrawerAdapter} from './adapter';
 import * as drawerUtil from './drawerUtil';
-import {AdapterDrawerDelegatePropType} from './types';
 import {BASE_CLASS_NAME} from './constants';
 
 export const CLASS_NAME = `${BASE_CLASS_NAME}__drawer`;
+
+const {
+  strings: {
+    FOCUSABLE_ELEMENTS
+  }
+} = MDCTemporaryDrawerFoundation;
 
 export type Props<P> = WrapperProps<P> & {
   className?: string
 };
 
 type State = {
-  translateX: ?number
+  foundationCssVars: Map<string, ?string>,
+  foundationEventListeners: Map<string, Set<EventListener>>
 };
 
 type Context = {
-  adapterDrawerDelegate: AdapterDrawerDelegate
+  adapter: FoundationAdapter
 };
 
 export default class Drawer<P: any> extends PropWrapper<*, P, *> {
@@ -34,7 +49,7 @@ export default class Drawer<P: any> extends PropWrapper<*, P, *> {
   context: Context
 
   static contextTypes = {
-    adapterDrawerDelegate: AdapterDrawerDelegatePropType.isRequired
+    adapter: React.PropTypes.instanceOf(FoundationAdapter).isRequired
   }
 
   static defaultProps = {
@@ -42,22 +57,16 @@ export default class Drawer<P: any> extends PropWrapper<*, P, *> {
   }
 
   state = {
-    translateX: null
-  }
-
-  adapterCallback: AdapterDrawerCallback = {
-    setTranslateX: (value: number) => {
-      this.setState({translateX: value});
-    },
-    getDOMNode: () => ReactDOM.findDOMNode(this)
+    foundationCssVars: new Map(),
+    foundationEventListeners: new Map()
   }
 
   componentDidMount () {
-    this.context.adapterDrawerDelegate.setCallback(this.adapterCallback);
+    this.context.adapter.setDrawerAdapter(new DrawerAdapterImpl(this));
   }
 
   componentWillUnmount () {
-    this.context.adapterDrawerDelegate.unsetCallback(this.adapterCallback);
+    this.context.adapter.setDrawerAdapter(new DrawerAdapter());
   }
 
   handleClick = (evt: SyntheticEvent) => {
@@ -68,17 +77,6 @@ export default class Drawer<P: any> extends PropWrapper<*, P, *> {
 
   getRootDOMNode (): HTMLElement {
     return ReactDOM.findDOMNode(this);
-  }
-
-  // Sync dom with state
-  componentDidUpdate (_prevProps: Props<P>, prevState: State) {
-    const rootNode = this.getRootDOMNode();
-    if (this.state.translateX == null) {
-      rootNode.style.removeProperty(drawerUtil.getTransformPropertyName());
-    } else if (this.state.translateX !== prevState.translateX) {
-      const value = this.state.translateX;
-      rootNode.style.setProperty(drawerUtil.getTransformPropertyName(), `translateX(${value}px)`);
-    }
   }
 
   renderProps (): P {
@@ -98,5 +96,79 @@ export default class Drawer<P: any> extends PropWrapper<*, P, *> {
       className
     };
     return props;
+  }
+
+  render (): React.Element<*> {
+    return (
+      <NativeDOMAdapter
+        cssVariables={this.state.foundationCssVars.toJS()}
+        eventListeners={this.state.foundationEventListeners.toJS()}>
+        {super.render()}
+      </NativeDOMAdapter>
+    );
+  }
+}
+
+class DrawerAdapterImpl extends DrawerAdapter {
+  element: Drawer<*>
+
+  constructor (element: Drawer<*>) {
+    super();
+    this.element = element;
+  }
+
+  registerDrawerInteractionHandler (evt: string, handler: EventListener) {
+    // Don't use click event handler of MDCTemporaryDrawerFoundation
+    // See `TemporaryDrawer.handleClick()` for more detail.
+    if (evt === 'click') {
+      return;
+    }
+    this.element.setState((state) => ({
+      foundationEventListeners: state.foundationEventListeners.update(
+        drawerUtil.remapEvent(evt, window),
+        new OrderedSet(),
+        x => x.add(handler)
+      )
+    }));
+  }
+  deregisterDrawerInteractionHandler (evt: string, handler: EventListener) {
+    // Don't use click event handler of MDCTemporaryDrawerFoundation
+    // See `TemporaryDrawer.handleClick()` for more detail.
+    if (evt === 'click') {
+      return;
+    }
+    this.element.setState((state) => ({
+      foundationEventListeners: state.foundationEventListeners.update(
+        drawerUtil.remapEvent(evt, window),
+        new OrderedSet(),
+        x => x.delete(handler)
+      )
+    }));
+  }
+  hasNecessaryDom (): boolean {
+    return this.getDOMNode() != null;
+  }
+  getDrawerWidth (): number {
+    return this.getDOMNode().getBoundingClientRect().width;
+  }
+  setTranslateX (value: number) {
+    if (value == null) {
+      this.element.setState((state) => ({
+        foundationCssVars: state.foundationCssVars.delete(drawerUtil.getTransformPropertyName())
+      }));
+    } else {
+      this.element.setState((state) => ({
+        foundationCssVars: state.foundationCssVars.set(drawerUtil.getTransformPropertyName(), `translateX(${value}px)`)
+      }));
+    }
+  }
+  isDrawer (el: Element): boolean {
+    return this.getDOMNode() === el;
+  }
+  getFocusableElements (): Iterable<Node> {
+    return this.getDOMNode().querySelectorAll(FOCUSABLE_ELEMENTS);
+  }
+  getDOMNode (): HTMLElement {
+    return ReactDOM.findDOMNode(this.element);
   }
 }
