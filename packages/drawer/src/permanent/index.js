c5703e59376a61d8211eefe24eb7e0fb8363612c
Choi Geonu <6566gun@gmail.com>
2017-02-15 10:32:09 +0900

    Remove unnecessary mdc-permanent-drawer__drawer component

--- packages/drawer/src/permanent/index.js
+++ packages/drawer/src/permanent/index.js
@@ -1,5 +1,4 @@
 /* @flow */
 export {default as PermanentDrawer} from './container';
-export {default as Drawer} from './drawer';
 export {default as ToolbarSpacer} from './toolbar-spacer';
 export {default as Content} from './content';
