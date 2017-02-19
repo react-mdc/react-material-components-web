4dd04c54884c5a22a0e60b55e50729b375c17528
Choi Geonu <6566gun@gmail.com>
2017-02-20 00:47:56 +0900

    Load mdc css without modification in demo

--- demo/src/js/index.js
+++ demo/src/js/index.js
@@ -6,6 +6,7 @@ import ReactDOM from 'react-dom';
 import MainRouter from './routes';
 
 import '../style/index.css';
+import 'material-components-web/dist/material-components-web.css';
 
 let element = document.getElementById('root');
 ReactDOM.render(<MainRouter />, element);
