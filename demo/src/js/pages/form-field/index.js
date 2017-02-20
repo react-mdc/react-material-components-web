633b35d6d1637953651dd72a7ac61c295b92807c
Choi Geonu <6566gun@gmail.com>
2017-02-21 00:10:27 +0900

    Remove warnings

--- demo/src/js/pages/form-field/index.js
+++ demo/src/js/pages/form-field/index.js
@@ -2,23 +2,9 @@
 import React from 'react';
 
 import {FormField} from '@react-mdc/form-field';
-import {Title, Typography} from '@react-mdc/typography';
 
 import PageTitle from 'app/js/components/page-title';
 import {Section, SectionTitle} from 'app/js/components/section';
-
-function FieldSet (props: *): * {
-  let {style} = props;
-  style = {
-    ...style,
-    margin: 16
-  };
-  return (
-    <fieldset
-      {...props}
-      style={style} />
-  );
-}
 
 export default class FormFieldExample extends React.Component {
   render (): React.Element<*> {
