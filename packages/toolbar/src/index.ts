c0e0a82fd1793e7620f35260a3529e96ed98d954
Choi Geonu <6566gun@gmail.com>
2017-04-08 18:32:42 +0900

    Add toolbar row component

--- packages/toolbar/src/index.ts
+++ packages/toolbar/src/index.ts
@@ -1,4 +1,5 @@
-export {default as Toolbar} from "./container";
+export {default as Container, default as Toolbar} from "./container";
+export {default as Row} from "./row";
 export {default as Section} from "./section";
 export {default as Title} from "./title";
 export {default as FixedToolbarAdjusted} from "./fixed-toolbar-adjusted";
